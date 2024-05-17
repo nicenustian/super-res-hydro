7d5b79f2ff49162d8b7b00462e53b3ba8405b298
Fahad Nasir <111900566+nicenustian@users.noreply.github.com>
2024-05-17 14:21:26 +0200

    Update utility_functions.py

--- utility_functions.py
+++ utility_functions.py
@@ -23,13 +23,13 @@ def set_seed(seed=123):
 ##########################Loss function####################################
 
 
-# reduce difference between scores and real and fake images
+# reduce difference between scores and real and fake field
 @tf.function
 def discriminator_loss(real_logits, fake_logits):
     return tf.reduce_mean(fake_logits) - tf.reduce_mean(real_logits)
 
 
-# maximize the expectation of the critic's output on fake images
+# maximize the expectation of the critic's output on fake field
 # opposite of discriminator score
 @tf.function
 def generator_loss(fake_logits_list):
@@ -43,25 +43,22 @@ def generator_loss(fake_logits_list):
 
 
 @tf.function(reduce_retracing=True)
-def gradient_penalty(discriminator, real, fake, image):
+def gradient_penalty(discriminator, real, fake):
         
     local_batch_size = tf.shape(real)[0]
 
-    # Get the interpolated  1D data / 2D slices
-    if image:
-        alpha = tf.random.uniform([local_batch_size, 1, 1, 1], 0, 1)
-    else:
-        alpha = tf.random.uniform([local_batch_size, 1, 1], 0, 1)
+    # Get the interpolated  1D data
+    alpha = tf.random.uniform([local_batch_size, 1, 1], 0, 1)
 
     interpolated = real + alpha * (fake - real)
         
     with tf.GradientTape() as gp_tape:
         gp_tape.watch(interpolated)
                         
-        # 1. Get the discriminator output for this interpolated image.
+        # 1. Get the discriminator output for this interpolated field.
         pred = discriminator(interpolated, training=True)
                     
-    # 2. Calculate the gradients w.r.t to this interpolated image.
+    # 2. Calculate the gradients w.r.t to this interpolated field.
     grads = gp_tape.gradient(pred, [interpolated])[0]
     # 3. Calculate the norm of the gradients all spatial dims, excluding batch
     axes_to_mean = tuple(range(1, len(real.shape)))
@@ -75,7 +72,7 @@ def gradient_penalty(discriminator, real, fake, image):
 
 # PDF loss function 
 @tf.function
-def pdf_loss_fn(real_list, fake_list, image=False):
+def pdf_loss_fn(real_list, fake_list):
         
     avg_pdf_loss = 0.0
     
@@ -94,18 +91,13 @@ def pdf_loss_fn(real_list, fake_list, image=False):
 
 
 @tf.function
-def ps_loss_fn(real_list, fake_list, box_size_list, image=False):
+def ps_loss_fn(real_list, fake_list, box_size_list):
         
     avg_ps_loss = 0.0
     for index, (real, fake) in enumerate(zip(real_list, fake_list)):
-        
-        if image:
-            real_k, real_ps = get_ps_fft2d(real, box_size_list[index])
-            fake_k, fake_ps = get_ps_fft2d(fake, box_size_list[index])
-    
-        else:
-            real_k, real_ps = get_ps_fft(real, box_size_list[index])
-            fake_k, fake_ps = get_ps_fft(fake, box_size_list[index])
+
+        real_k, real_ps = get_ps_fft(real, box_size_list[index])
+        fake_k, fake_ps = get_ps_fft(fake, box_size_list[index])
             
         avg_ps_loss += tf.reduce_mean(tf.math.abs(real_ps - fake_ps))
         
@@ -177,80 +169,6 @@ def get_ps_fft(data, skewer_length=20):
 
 
 
-@tf.function
-def get_ps_fft2d(data, skewer_length=20):
-    pi = tf.constant(np.pi, dtype=tf.float32)
-    samples = tf.shape(data)[0]
-    nbins = tf.shape(data)[1]
-    nfields = tf.shape(data)[3]  # Get the number of fields
-
-    ps_ta = tf.TensorArray(tf.float32, size=nfields)  # Create a TensorArray to store ps for each field
-    k_ta = tf.TensorArray(tf.float32, size=nfields) # Create a TensorArray to store ps for each field
-
-    for field_idx in tf.range(nfields):
-        field_data = data[...,field_idx]  # Select data for the current field
-                
-        ps = tf.zeros((nbins,nbins), dtype=tf.float32)  # Initialize ps with the correct shape
-
-        for i in tf.range(samples):
-            ps += tf.abs(tf.signal.fftshift(tf.signal.fft2d(
-                tf.cast(field_data[i], tf.complex64)
-                ))) ** 2
-
-        # Define the k-space grid
-        k_y = tf.range(-(nbins // 2), nbins // 2, dtype=tf.float32) / skewer_length
-        k_x = tf.range(-(nbins // 2), nbins // 2, dtype=tf.float32) / skewer_length
-        
-        # Create a meshgrid of k-space frequencies
-        k_y_grid, k_x_grid = tf.meshgrid(k_y, k_x, indexing='ij')        
-        k = 2 * pi * tf.sqrt(k_y_grid**2 + k_x_grid**2)
-        ps *= skewer_length**2 / tf.cast(samples, tf.float32)
-        ps *= k / pi
- 
-        
-        num_of_bins = tf.shape(k)[0] 
-        bin_size = (tf.reduce_max(k) - tf.reduce_min(k)) / tf.cast(
-            num_of_bins, dtype=tf.float32)
-          
-        # Compute mean ps for each bin
-        k_bins = tf.linspace(tf.reduce_min(k),
-                             tf.reduce_max(k), 
-                             num=num_of_bins)  # Define bins for k values
-        
-        mean_ps = tf.TensorArray(tf.float32, size=num_of_bins)
-        
-        for i in range(num_of_bins):
-            mask = tf.logical_and(k >= (k_bins[i] - bin_size/2.0), 
-                                  k < (k_bins[i] + bin_size/2.0))
-            mean_ps = mean_ps.write(i, tf.reduce_mean(tf.boolean_mask(ps, mask)))
-            
-        # Convert mean_ps to a tensor
-        mean_ps = mean_ps.stack()
-        
-        kmin = tf.reduce_min(k_bins[1:])
-        kmax = tf.reduce_max(k_bins[1:])
-        
-        k_interp = tf.cast(tf.linspace(
-            tf.experimental.numpy.log10(kmin),
-            tf.experimental.numpy.log10(kmax), 
-            100), 
-            dtype=tf.float32)
-
-        # Interpolate power spectrum to desired k values
-        ps_interp = tf.experimental.numpy.log10(
-            tfp.math.interp_regular_1d_grid(
-            x=tf.math.pow(10., k_interp),
-            x_ref_min=kmin, 
-            x_ref_max=kmax, 
-            y_ref=mean_ps[1:],
-        ))
-        
-    
-        ps_ta = ps_ta.write(field_idx, ps_interp)  # Write ps_interp to TensorArray
-        k_ta = k_ta.write(field_idx, k_interp)  # Write ps_interp to TensorArray
-
-    return k_ta.stack(), ps_ta.stack()  # Return k values and power spectra for all fields
-
 
 
 @tf.function
@@ -301,7 +219,7 @@ def get_pdf(data):
 
 
 @tf.function
-def scale_fake_examples(fake_list, original_dim, batch_size, image=False):
+def scale_fake_examples(fake_list, original_dim, batch_size):
     
     fake_reshaped_list = []
     
@@ -312,18 +230,10 @@ def scale_fake_examples(fake_list, original_dim, batch_size, image=False):
         num_features = tf.shape(fake)[-1]
         
         factor = fake_dim // original_dim
-
         
-        if not image:
-            new_dim = fake_dim // factor
-            fake_reshape = tf.reshape(fake, (batch_size * factor, new_dim, num_features))
-        elif image:
-            new_dim = fake_dim // factor
-            fake_reshape = tf.reshape(fake, (batch_size * factor * factor, new_dim, new_dim, num_features))
-        else:
-            raise ValueError("Unsupported shape. Only 3D and 4D tensors are supported.")
-            
-                
+        new_dim = fake_dim // factor
+        fake_reshape = tf.reshape(fake, (batch_size * factor, new_dim, num_features))
+              
         indices = tf.random.shuffle(tf.range(tf.shape(fake_reshape)[0]))[:batch_size]
         fake_sample = tf.gather(fake_reshape, indices)    
         fake_reshaped_list.append(fake_sample)
